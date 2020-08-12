959c6eceadd168ace273b0452148b20f0246a862
Gerard Falco <gerardfp@users.noreply.github.com>
2020-08-12 11:13:51 +0200

    Create new_post.php

---
+++ new_post.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="style.css">
+
+<form id="form" action="insert_post.php" method="get">
+        <label for="title">Titulo:</label>
+        <input id="title" type="text" name="title" size="46" required/>
+        <label for="content">Contenido:</label>
+        <textarea id="content" type="text" name="content" rows="8" cols="64" required></textarea>
+        <input id="submit" type="submit" value="Crear post" disabled/>
+</form>
+
+<script>
+document.getElementById('form').addEventListener("keyup", () => {
+        document.getElementById("submit").disabled = !form.checkValidity();
+});
+</script>
