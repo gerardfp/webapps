cc66c21c4d1ac4223a54e6f575af2db86f8e8cef
root <root@myblog.lxd>
2020-08-12 09:58:20 +0000

    --

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <link rel="stylesheet" href="style.css">
+
 <h1>MY BLOG!</h1>
 <a href='new_post.php'>Crear nuevo post</a>
 
 <?php
 $mysqli = new mysqli("localhost", "my_user", "my_password", "my_database");
 
-$res = $mysqli->query("SELECT * FROM posts");
+$result = $mysqli->query("SELECT * FROM posts");
 
-$res->data_seek(0);
-while ($row = $res->fetch_assoc()) {
+while ($row = $result->fetch_assoc()) {
     echo "<h2>" . $row['title'] . "</h2>\n";
     echo "<p>" . $row['content'] . "</p>\n";
 }
